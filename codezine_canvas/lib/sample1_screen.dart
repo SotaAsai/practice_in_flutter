0436f667af19e5a5a7f6c7a0af549485275f7310
SotaAsai <sota.asai.28@gmail.com>
2024-05-12 13:01:34 +0900

    少し修正

--- codezine_canvas/lib/sample1_screen.dart
+++ codezine_canvas/lib/sample1_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-class _Sample1Screen extends State<Sample1Screen>{
+class Sample1Screen extends StatefulWidget {
+  const Sample1Screen({super.key});
 
+  @override
+  State createState() {
+    return _Sample1Screen();
+  }
+}
+
+class _Sample1Screen extends State<Sample1Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar : AppBar(
-        title : const Text("サンプル(1)")
-      ),
+      appBar: AppBar(title: const Text("サンプル(1)")),
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
@@ -19,42 +25,29 @@ class _Sample1Screen extends State<Sample1Screen>{
   }
 }
 
-class Sample1Screen extends StatefulWidget{
-
-  const Sample1Screen({super.key});
-
-  @override
-  State createState() {
-    return _Sample1Screen();
-  }
-}
-
-class SamplePainter extends CustomPainter{
-
+class SamplePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-
     // サイズの値は画面サイズ
     print("size is {$size}");
 
-    Paint paint = Paint()
-        ..color = Colors.red;
+    Paint paint = Paint()..color = Colors.red;
 
-    //  マージン10にでの画面一杯の四角
-    Rect rect1 = const Offset(20.0, 20.0) & Size(size.width - 40.0,size.height - 40.0);
+    //  マージン10での画面一杯の四角
+    Rect rect1 =
+        const Offset(20.0, 20.0) & Size(size.width - 40.0, size.height - 40.0);
     canvas.drawRect(rect1, paint);
 
     //
-    Paint paint2 = Paint()
-      ..color = Colors.blue;
+    Paint paint2 = Paint()..color = Colors.blue;
 
-    Rect rect2 = Rect.fromLTWH(size.width / 2 - 50.0, size.height / 2 - 50.0, 100.0, 100.0);
+    Rect rect2 = Rect.fromLTWH(
+        size.width / 2 - 50.0, size.height / 2 - 50.0, 100.0, 100.0);
     canvas.drawRect(rect2, paint2);
 
-    Paint paint3 = Paint()
-      ..color = Colors.black;
+    Paint paint3 = Paint()..color = Colors.black;
 
-    canvas.drawCircle(Offset(size.width/2,size.height/2), 40, paint3);
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 40, paint3);
   }
 
   @override
