5d78ed31d219e1586200dd0e5212a8fe8344a5e0
SotaAsai <sota.asai.28@gmail.com>
2025-10-19 14:12:24 +0900

    【shell_route】下タブを切り替えてもbuildは再実行されてないを確認

--- shell_route/lib/details_screen.dart
+++ shell_route/lib/details_screen.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key, required this.label});
-  
+
   final String label;
-  
+
   @override
   Widget build(BuildContext context) {
+    // 下タブを切り替えてもbuildは再実行されてない
+    print('DetailScreenのビルド:$label');
     return Center(child: Text('$label Details'));
   }
 }
--- shell_route/lib/root_screen.dart
+++ shell_route/lib/root_screen.dart
@@ -3,12 +3,14 @@ import 'package:go_router/go_router.dart';
 
 class RootScreen extends StatelessWidget {
   const RootScreen({super.key, required this.label, required this.detailsPath});
-  
+
   final String label;
   final String detailsPath;
-  
+
   @override
   Widget build(BuildContext context) {
+    // 下タブを切り替えてもbuildは再実行されてない
+    print('RootScreenのビルド:$label');
     return Center(
       child: ElevatedButton(
         onPressed: () => context.go(detailsPath),
