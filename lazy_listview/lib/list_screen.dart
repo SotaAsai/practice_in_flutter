f0e5c695ada2eab887c6d24ed947be004acbba48
SotaAsai <sota.asai.28@gmail.com>
2025-10-20 13:51:08 +0900

    【lazy_listview】2回目のフェッチからはフェッチするアイテム数を固定しない

--- lazy_listview/lib/list_screen.dart
+++ lazy_listview/lib/list_screen.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+const itemViewHeight = 50.0;
+
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
 
@@ -23,10 +25,10 @@ class _ListScreenState extends State<ListScreen> {
     super.initState();
     _isLoading = true;
     _hasMore = true;
-    _loadMore();
+    _loadFirst();
   }
 
-  void _loadMore() {
+  void _loadFirst() {
     _isLoading = true;
     _itemFetcher.fetch().then((List<WordPair> fetchedList) {
       if (fetchedList.isEmpty) {
@@ -43,8 +45,27 @@ class _ListScreenState extends State<ListScreen> {
     });
   }
 
+  void _loadMore(double windowHeight) {
+    _isLoading = true;
+    _itemFetcher.fetchMore(windowHeight).then((List<WordPair> fetchedList) {
+      if (fetchedList.isEmpty) {
+        setState(() {
+          _isLoading = false;
+          _hasMore = false;
+        });
+      } else {
+        setState(() {
+          _isLoading = false;
+          _pairList.addAll(fetchedList);
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('ビルド');
+    print('ウィンドウの高さ:${MediaQuery.sizeOf(context).height}');
     return ListView.builder(
       // Need to display a loading tile if more items are coming
       itemCount: _hasMore ? _pairList.length + 1 : _pairList.length,
@@ -53,19 +74,23 @@ class _ListScreenState extends State<ListScreen> {
         // print('ListView.builder is building index $index');
         if (index >= _pairList.length) {
           if (!_isLoading) {
-            _loadMore();
+            double windowHeight = MediaQuery.sizeOf(context).height;
+            _loadMore(windowHeight);
           }
           return Center(
             child: SizedBox(
-              child: CircularProgressIndicator(),
               height: 24,
               width: 24,
+              child: CircularProgressIndicator(),
             ),
           );
         }
-        return ListTile(
-          leading: Text(index.toString(), style: _biggerFont),
-          title: Text(_pairList[index].asPascalCase, style: _biggerFont),
+        return SizedBox(
+          height: itemViewHeight,
+          child: ListTile(
+            leading: Text(index.toString(), style: _biggerFont),
+            title: Text(_pairList[index].asPascalCase, style: _biggerFont),
+          ),
         );
       },
     );
@@ -80,7 +105,28 @@ class _ItemFetcher {
   // This async function simulates fetching results from Internet, etc.
   Future<List<WordPair>> fetch() async {
     final list = <WordPair>[];
+
+    // double windowHeight = MediaQuery.sizeOf(context).height;
+    // print('windowHeight:$windowHeight');
+
     final n = min(_itemsPerPage, _count - _currentPage * _itemsPerPage);
+    // Uncomment the following line to see in real time now items are loaded lazily.
+    // print('Now on page $_currentPage');
+    await Future.delayed(Duration(seconds: 1), () {
+      for (int i = 0; i < n; i++) {
+        list.add(WordPair.random());
+      }
+    });
+    _currentPage++;
+    return list;
+  }
+
+  Future<List<WordPair>> fetchMore(double windowHeight) async {
+    final list = <WordPair>[];
+
+    final itemsPerPage = windowHeight ~/ itemViewHeight + 5;
+
+    final n = min(itemsPerPage, _count - _currentPage * itemsPerPage);
     // Uncomment the following line to see in real time now items are loaded lazily.
     // print('Now on page $_currentPage');
     await Future.delayed(Duration(seconds: 1), () {
