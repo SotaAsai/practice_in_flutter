79d1ba195ec037794ac725dade2119bdadd03b2a
SotaAsai <sota.asai.28@gmail.com>
2025-10-22 11:05:13 +0900

    【various_scrollviews】RefreshIndicatorAndListviewウィジェットにScaffoldを追加

--- various_scrollviews/lib/refresh_indicator_and_listview.dart
+++ various_scrollviews/lib/refresh_indicator_and_listview.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class RefreshIndicatorAndListview extends StatelessWidget {
@@ -6,6 +5,9 @@ class RefreshIndicatorAndListview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('テスト')),
+      body: const Placeholder(),
+    );
   }
 }
